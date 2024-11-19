46c9c88f658046f43e6ef11928c8edf2e1718a26
FirstTimeEZ <179362031+FirstTimeEZ@users.noreply.github.com>
2024-11-19 19:15:17 +1300

    add node server

--- README.md
+++ README.md
@@ -1,3 +1,55 @@
-# Responsive Website Design
+# Responsive Website
 
-A responsive dark theme made with pure `HTML` and `CSS`
+A simple responsive website made with pure `HTML` and `CSS` and `Javscript`
+
+# Server
+
+### Start
+
+You can start the `server` with `Node.js`
+
+```
+node server.js
+```
+
+### 404/500 Pages
+
+The server is configured to serve custom `404` and `500` error pages. 
+
+If you want to add more specific custom error pages, place them in the `/error` folder.
+
+### Port
+
+By default, the server runs on port `3000`. 
+
+If you want to change the port, you can set the `PORT` environment variable in your `Node.js` environment:
+
+```js
+const PORT = process.env.PORT || 3000;
+```
+
+# Structure
+
+The website's files are organized within the `/website` folder, which contains subdirectories for images, JavaScript files, and styles, along with an index.html file that serves as the main entry point.
+ 
+```
+/root
+│
+├── /error
+│   ├── 404.html
+│   └── 500.html
+│
+├── /website
+│   ├── /imgs
+│   ├── /js
+│   │   ├── /exports
+│   │   │   └── tags.js
+│   │   └── main.js
+│   ├── /style
+│   │   └── responsive.css
+│   └── index.html
+│
+└── server.js
+```
+
+The `index.html` file uses `js/main.js`, which is responsible for loading the JavaScript modules from the `js/exports` folder.
---
+++ server.js
@@ -0,0 +1,84 @@
+import { createServer } from 'http';
+import { readFile } from 'fs';
+import { fileURLToPath } from 'url';
+import { join, extname as _extname, dirname  } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const server = createServer((req, res) => {
+    let filePath = join(__dirname, 'website', req.url === '/' ? 'index.html' : req.url);
+
+    const extname = _extname(filePath);
+
+    let contentType;
+
+    switch (extname) {
+        case '.html':
+            contentType = 'text/html';
+            break;
+        case '.css':
+            contentType = 'text/css';
+            break;
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.json':
+            contentType = 'application/json';
+            break;
+        case '.png':
+            contentType = 'image/png';
+            break;
+        case '.jpg':
+            contentType = 'image/jpg';
+            break;
+        case '.gif':
+            contentType = 'image/gif';
+            break;
+        case '.svg':
+            contentType = 'image/svg+xml';
+            break;
+        case '.ico':
+            contentType = 'image/x-icon';
+            break;
+    }
+
+    readFile(filePath, (err, content) => {
+        if (err) {
+            if (err.code === 'ENOENT') {
+                readFile(join(__dirname, '/error/404.html'), (err404, content404) => {
+                    if (err404) {
+                        res.writeHead(500);
+                        res.end('Server Error');
+                    } else {
+                        res.writeHead(404, { 'Content-Type': 'text/html' });
+                        res.end(content404);
+                    }
+                });
+            } else {
+                readFile(join(__dirname, '/error/500.html'), (error500, content500) => {
+                    if (error500) {
+                        res.writeHead(500);
+                        res.end('Server Error');
+                    } else {
+                        res.writeHead(500, { 'Content-Type': 'text/html' });
+                        res.end(content500);
+                    }
+                });
+            }
+            return;
+        }
+
+        res.writeHead(200, { 'Content-Type': contentType });
+        res.end(content);
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, (err) => {
+    if (err) {
+        console.error('Error starting server:', err);
+        return;
+    }
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
